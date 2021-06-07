9696590e447e6dd1b2462a4b92fa3f3db5c4a17c
EvgeniyaSimakova <elustach@yahoo.com>
2021-06-07 21:08:44 +0300

    Homeworks

--- Hometask10_работа с массивами_часть2/script.js/script.js
+++ Hometask10_работа с массивами_часть2/script.js/script.js
@@ -1,22 +1,26 @@
 // Task 1 Написать функцию, принимающую массив имен и возвращающую массив объектов вида {name: 'Vasya'}
 
-function changeNames(names) {
-    return names.map(function(curr) {
-        return  {'name': curr};
-    });
+var names = ['Vasya', 'John', 'Peter', 'David', 'Daniel'];
+
+function changeNames(curr) {
+    return '{name:' + curr +'}';
 }
 
-console.log(changeNames(['Vasya', 'John', 'Peter', 'David', 'Daniel']));
+console.log(names.map(changeNames));
 
 // Task 2 
 
-function currentTime(time) {
-    return 'Текущее время: '+ time.reduce(function(result, currentElem) {
-        return result  + ' : ' + currentElem;
+var currentArr = ['00', '13', '24'];
+
+function currentTime(arr) {
+    var time = arr.reduce(function(result, currentElem) {
+        return  result  + ' : ' + currentElem;
     });
+
+    return 'Текущее время: ' + time;
 }
 
-console.log(currentTime(['00', '13', '24']));
+console.log(currentTime(currentArr));
 
 // Task 3 подсчет гласных в слове
 
